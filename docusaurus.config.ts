a77af32e8780266286e9065ec768a146097b9f05
Mauriceac <maurice.ac@gmail.com>
2025-09-03 19:49:31 -0400

    search plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -156,7 +156,7 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
-  // plugins: [require.resolve('docusaurus-lunr-search')],
+  plugins: [require.resolve('docusaurus-lunr-search')],
 };
 
 export default config;
